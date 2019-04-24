9dc0118e3de5d4b9b602b6e04c8f82d0c7642384
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-24 13:40:38 -0700

    state storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
 import "./App.css";
 
+const toDoItems = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      toDoItemsOnState: toDoItems,
+      item: {
+        task: '',
+        id: '',
+        completed: ''
+      }
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
